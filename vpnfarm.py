ca134e2f3242fc57a004f874ae83b74d86859965
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2015-07-11 11:51:07 +0200

    auto-mode on server

--- vpnfarm.py
+++ vpnfarm.py
@@ -25,15 +25,40 @@ class VPNStatus(object):
 class FarmServer(object):
   def __init__(self,mode):
     # Default: check that everything is working fine
-    if mode = "":
-      do_check_and_connect()
+    if mode == '':
+      self.do_check_and_connect()
+    elif mode == 'stop':
+      self.do_stop()
 
-  def do_check_and_connect(self)
+  def do_check_and_connect(self):
     self.pid = self.get_pid()
     if self.pid == "":
-#####TODO: Start the server
-        print "The VPN Server is not running! Please start the service first."
-        return
+      start_command = subprocess.Popen('/etc/init.d/openvpnas start',
+                           shell=True,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+      start_result, start_err = start_command.communicate() 
+      start_pid = self.get_pid()   
+      print('The VPN Server was not running! The new PID is ' + start_pid)
+      return
+
+  def do_stop(self):
+    self.pid = self.get_pid()
+    if self.pid == "":
+      print('The VPN Server was not running! (nothing had to be stopped)')
+      return
+    else:
+      start_command = subprocess.Popen('/etc/init.d/openvpnas stop',
+                           shell=True,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+      start_result, start_err = start_command.communicate() 
+      start_pid = self.get_pid()   
+      if start_pid == '':
+        print('The VPN Server has been stopped!')
+      else:
+        print('Something weird happened, the VPN Server has been stopped but some processes are still running. PLEASE CHECK!')
+      return
 #####TODO: Add more options
 
 
@@ -68,7 +93,6 @@ class FarmClient(object):
   def start_newclient(self):
     master, slave = pty.openpty()
     start_command = subprocess.Popen('/usr/sbin/openvpn /etc/openvpn/client_vpnfarm.ovpn > /etc/openvpn/openvpn.log &',
-                           #shell=True, stdin=subprocess.PIPE, stdout=slave, stderr=slave, close_fds=True)
                            shell=True, stdout=slave, stderr=slave, close_fds=True)
     result = os.fdopen(master)
 
@@ -77,9 +101,10 @@ def show_error():
   print(sys.argv[0] + ' [server|client] <mode>')
   print('        , where mode can be')
   print('        1.- for the SERVER machine:')
-  print('          - <nothing>, also known as auto-mode. The client makes himself available to the openvpn server.')
+  print('          - <nothing>, also known as auto-mode. The server gets everything ready for the clients that are connected.')
+  print('          - stop, the server gets stopped if it\'s running.')
   print('        2.- for the CLIENT machine:')
-  print('          - <nothing>, again known as auto-mode. The server gets everything ready for the clients that are connected.')
+  print('          - <nothing>, again, auto-mode. The client makes himself available to the openvpn server.')
 
 if __name__ == '__main__':
   try:
